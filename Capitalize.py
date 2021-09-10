4b23f49e8cc9d3fa06873ce1b722427508a6044e
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-10 11:33:16 +0530

    Update Capitalize.py

--- Capitalize.py
+++ Capitalize.py
@@ -1,3 +1,4 @@
+### Method 1
 def solve(s):
   for i in s.split():
     s = s.replace(i,i.capitalize())
@@ -9,3 +10,13 @@ if __name__ = "__main__":
   result = solve(s)
   fptr.write(result + "\n")
   fptr.close()
+  
+###Method 2
+def solve_it(s):
+    for i in s.split():
+        s = s.title()
+    return s
+
+s = input()
+result = solve_it(s)
+print(result)
